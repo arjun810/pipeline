785f29aee1b3ea251a9cee510c203e880f0ce1d3
Arjun Singh <arjun810@gmail.com>
2014-02-27 22:10:05 -0800

    dependencies

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name="ziang",
     packages=['ziang'],
     version = "0.1.0a",
     description = "Task pipeline system",
+    install_requires=["networkx", "cloud"],
+    dependency_links=["git+git://github.com/arjun810/lucidity.git"],
 )
